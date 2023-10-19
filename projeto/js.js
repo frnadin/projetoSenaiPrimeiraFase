66466679cfdfb167e8a310b3121203f256d7332e
frnadin <127996959+frnadin@users.noreply.github.com>
2023-10-18 21:55:04 -0300

    Add files via upload
    18-10

--- projeto/js.js
+++ projeto/js.js
@@ -1,24 +1,39 @@
 
-
-
 let participantes = []
 
 for(var i=0;i!=100;i++){
   participantes.push("null")
-  
-}
+  }
+
 console.log(participantes.length)
 
+
+
 function cadastro() {
-  let indiceCadastro = Number(prompt("num de 0 a 100")) 
+  let indiceCadastro = Number(prompt("num de 0 a 99")) 
+  if(indiceCadastro<100){
   let nomeCadastrado = prompt("Digite o nome do participante que vc quer cadastrar.")
   
   participantes[indiceCadastro]=nomeCadastrado
   console.log(participantes)
+  alert("Cadastrado!")
+}else{
+  alert("Entrada invalida!")
+}
   }
 
-  function numSort(){
+  function numSort(){//ARRUMAR  CASO SELECIONE MAIOR QUE 0100
     let sorte = Number(prompt("Qual o numero sorteado?"))
+    if(sorte<100){
     let sorteado = participantes[sorte] 
-    return alert(sorteado)
+    return  document.getElementById("saida").innerHTML = "Pessoa sorteada: "+sorteado
+    }else{
+      alert("entrada invalida")
+    }
   }
+  
+  function limpar(){  
+    return document.getElementById("saida").innerHTML = ""
+  }
+  
+ 
