8dac1c8cd907b8c411fcddb9beb05c04146cc730
frnadin <127996959+frnadin@users.noreply.github.com>
2023-10-24 21:07:57 -0300

    Add files via upload

--- projeto/js.js
+++ projeto/js.js
@@ -1,6 +1,7 @@
 
 let participantes = []
 let participantesCadastrados = []
+let numCadastrados = []
 
 for(var i=0;i!=100;i++){
   participantes.push("null")
@@ -16,14 +17,19 @@ function cadastro() {
  
   if(indiceCadastro<100){
     let nomeCadastrado = prompt("Digite o nome do participante que você quer cadastrar.")
+
+    if(participantes[indiceCadastro]=="null"){
+
     participantes[indiceCadastro]=nomeCadastrado
     console.log(participantes)
          alert("Cadastrado!")
 
-        participantesCadastrados.unshift(nomeCadastrado)
+        participantesCadastrados.unshift(String(indiceCadastro + " | "+nomeCadastrado))
 
         document.getElementById("br").innerHTML = participantesCadastrados.join("<br>")
-
+      
+        document.getElementById("brEsquerdo").innerHTML = participantesCadastrados.join("<br>")            
+    }else{alert("numero já ocupado")}
 }else{
   alert("Entrada invalida!")
 }
@@ -31,17 +37,22 @@ function cadastro() {
 
   function numSort(){
     let sorte = Number(prompt("Qual o numero sorteado?"))
-    if(sorte<100){
+    if(sorte<100 & participantes[sorte]!="null"){
     let sorteado = participantes[sorte] 
-    return  document.getElementById("saida").innerHTML = "Pessoa sorteada: "+sorteado
+      document.getElementById("saida").innerHTML = "Pessoa sorteada: "+sorteado +"<br>"+"Numero sorteado: "+sorte
+      
+
+    document.getElementById('aaa').style.visibility = 'visible'
+
+
     }else{
       alert("entrada invalida")
     }
   }
   
   function limpar(){  
-  document.getElementById("saida").innerHTML = ""
-
+ 
+  window.location.reload(true);
 
   }
 
